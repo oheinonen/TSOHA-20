9f3cfc10e60bf3c21802c169b6c3cb838c467a26
Heinonen Oskari L <heinonos@cubbli20-gold.local>
2020-09-15 11:03:56 +0300

    created sql tables roles, restarants & employees

--- schema.sql
+++ schema.sql
@@ -3,3 +3,25 @@ CREATE TABLE users (
   username TEXT UNIQUE;
   password TEXT
 );
+
+CREATE DOMAIN RoleDomain TEXT
+  CHECK (VALUE IN ('leipuri', 'kokki', 'kassahenkilö', 'tarjoilija', ' tiskari' ));
+
+CREATE TABLE roles (
+  id SERIAL PRIMARY KEY;
+  role RoleDomain;
+  restaurantID REFERENCES restaurants(id);
+  employeeID REFERENCES employee(id);
+)
+
+CREATE TABLE employees (
+  id SERIAL PRIMARY KEY;
+  name TEXT;
+  max_hours INT
+)
+
+CREATE TABLE restaurants (
+  id SERIAL PRIMARY KEY;
+  name TEXT;
+  roles RoleDomain
+);
